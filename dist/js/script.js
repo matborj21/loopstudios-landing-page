bd3fe72bef677e0cde54a4f715c4818e8428f3e0
matborj21 <omatborje@gmail.com>
2021-02-28 21:44:11 +0800

    finished navbar and hero sectoion with mobile responsive

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,8 @@
 const toggler = document.getElementById('toggler');
 const hamburger = document.getElementById('hamburger');
+const navbar = document.getElementById('navbar');
 
 toggler.addEventListener('click', () => {
     hamburger.classList.toggle('animate');
+    navbar.classList.toggle('show');   
 });
