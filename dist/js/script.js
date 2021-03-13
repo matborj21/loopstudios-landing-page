27bac305a19bff13a22c77f866c70957b8082335
matborj21 <omatborje@gmail.com>
2021-03-13 17:10:41 +0800

    change the design layout on about section

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,8 @@ const toggler = document.getElementById('toggler');
 const hamburger = document.getElementById('hamburger');
 const navbar = document.getElementById('navbar');
 
+
+
 toggler.addEventListener('click', () => {
     hamburger.classList.toggle('animate');
     navbar.classList.toggle('show');   
